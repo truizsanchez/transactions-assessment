37af9fe4199a2563796cfda0c286bcc635c2ad57
Tomas Ruiz <truizsanchez@gmail.com>
2025-07-17 21:55:46 +0200

    type hints

--- src/transactions/views/deposit_view.py
+++ src/transactions/views/deposit_view.py
@@ -1,8 +1,12 @@
+from decimal import Decimal
+
 from django.db import transaction
 from rest_framework import permissions, status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from transactions.models import Account
 from transactions.models.transaction import Transaction
 from transactions.serializers import TransactionSerializer
 
@@ -10,11 +14,11 @@ from transactions.serializers import TransactionSerializer
 class DepositView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         serializer = TransactionSerializer(data=request.data)
         if serializer.is_valid():
-            amount = serializer.validated_data['amount']
-            account = request.user.account
+            amount: Decimal = serializer.validated_data['amount']
+            account: Account = request.user.account
 
             with transaction.atomic():
                 account.balance += amount
@@ -25,6 +29,7 @@ class DepositView(APIView):
                     amount=amount
                 )
             return Response(
-                {'message': 'Deposit successful', 'new_balance': account.balance}, status=status.HTTP_200_OK
+                {'message': 'Deposit successful', 'new_balance': account.balance},
+                status=status.HTTP_200_OK
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
