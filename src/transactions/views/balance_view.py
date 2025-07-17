6a119841c575489eab6758c294d5989cc97dfe90
Tomas Ruiz <truizsanchez@gmail.com>
2025-07-17 21:59:39 +0200

    type hints

--- src/transactions/views/balance_view.py
+++ src/transactions/views/balance_view.py
@@ -1,14 +1,16 @@
 from rest_framework import permissions, status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from transactions.models import Account
 from transactions.serializers import BalanceSerializer
 
 
 class BalanceView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request):
-        account = request.user.account
+    def get(self, request: Request) -> Response:
+        account: Account = request.user.account
         serializer = BalanceSerializer(account)
         return Response(serializer.data, status=status.HTTP_200_OK)
--- src/transactions/views/withdraw_view.py
+++ src/transactions/views/withdraw_view.py
@@ -1,8 +1,13 @@
+from decimal import Decimal
+import time
+
 from django.db import transaction
 from rest_framework import permissions, status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from transactions.models import Account
 from transactions.models.transaction import Transaction
 from transactions.serializers import TransactionSerializer
 
@@ -10,16 +15,20 @@ from transactions.serializers import TransactionSerializer
 class WithdrawView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def post(self, request):
+    def post(self, request: Request) -> Response:
         serializer = TransactionSerializer(data=request.data)
         if serializer.is_valid():
-            amount = serializer.validated_data['amount']
-            account = request.user.account
+            amount: Decimal = serializer.validated_data['amount']
+            account: Account = request.user.account
 
             if account.balance < amount:
-                return Response({'error': 'Insufficient funds'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {'error': 'Insufficient funds'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
 
             with transaction.atomic():
+                time.sleep(0.2)  # Simulación de retardo (¿tal vez para condiciones de carrera?)
                 account.balance -= amount
                 account.save()
                 Transaction.objects.create(
@@ -28,6 +37,7 @@ class WithdrawView(APIView):
                     amount=amount
                 )
             return Response(
-                {'message': 'Withdrawal successful', 'new_balance': account.balance}, status=status.HTTP_200_OK
+                {'message': 'Withdrawal successful', 'new_balance': account.balance},
+                status=status.HTTP_200_OK
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
